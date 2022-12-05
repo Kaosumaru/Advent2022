3b4f8fba36d63d7fc1c3fa5e6d11133a32cdbda8
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-06 00:04:23 +0100

    Added Exercise1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿IEnumerable<int> GetElfCalories(StreamReader input)
+{
+    while (true)
+    {
+        var line = input.ReadLine();
+        if (string.IsNullOrEmpty(line))
+            yield break;
+        yield return int.Parse(line);
+    }
+}
+
+IEnumerable<IEnumerable<int>> GetEveryElfCalories(StreamReader input)
+{
+    while (true)
+    {
+        if (input.EndOfStream)
+            yield break;
+        yield return GetElfCalories(input);
+    }
+}
+
+string path = "../../../data2.txt";
+
+using (StreamReader file = new(path))
+{
+    var maxElfCalories = GetEveryElfCalories(file)
+        .Select(elf => elf.Sum())
+        .Max();
+    Console.WriteLine(maxElfCalories);
+}
