315d9c3a776ece30fc2dcd398c9f15c18e29aac2
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-21 11:08:51 +0100

    Add Day19 part2.

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,23 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Day19;
+using System.Text.RegularExpressions;
 using Utils;
 
-Console.WriteLine("Hello, World!");
+Blueprint StringToBluePrint(string str)
+{
+    var robots = str.Split(". ");
+    //var regex = 
 
-Blueprint bp = new Blueprint();
-bp.Costs[Resources.ORE][Resources.ORE] = 4;
+    Regex[] rg = new Regex[]
+    {
+        new(@"(\d+) ore"),
+        new(@"(\d+) clay"),
+        new(@"(\d+) obsidian")
+    };
 
-bp.Costs[Resources.CLAY][Resources.ORE] = 2;
+    Blueprint bp = new Blueprint();
 
-bp.Costs[Resources.OBSIDIAN][Resources.ORE] = 3;
-bp.Costs[Resources.OBSIDIAN][Resources.CLAY] = 14;
+    int robotIndex = 0;
+    foreach (var robot in robots)
+    {
+        int resIndex = 0;
+        foreach (var regex in rg)
+        {
+            var mt = regex.Match(robot);
+            if (!mt.Success)
+            {
+                resIndex++;
+                continue;
+            }
 
-bp.Costs[Resources.GEODE][Resources.ORE] = 2;
-bp.Costs[Resources.GEODE][Resources.OBSIDIAN] = 7;
+            bp.Costs[robotIndex][resIndex] = int.Parse(mt.Groups[1].Value);
+            resIndex++;
+        }
 
-var start = Move.CreateStartingMove(bp, 24);
-MoveSolver<Move> solver = new();
-solver.FindPathFrom(start);
+        robotIndex++;
+    }
+    return bp;
+}
 
-var move = solver.BestMove;
-Console.WriteLine(solver.BestMove.GetScore());
+
+long ScoreForBluePrint(Blueprint bp, int time)
+{
+    var start = Move.CreateStartingMove(bp, time);
+    MoveSolver<Move> solver = new();
+    solver.FindPathFrom(start);
+
+    return solver.BestMove.GetScore();
+}
+
+void Exercise1(string path)
+{
+    long totalScore = 0;
+    long index = 1;
+    foreach (var line in File.ReadLines(path))
+    {
+        Console.WriteLine($"Calculating for {index}");
+
+        var bp = StringToBluePrint(line);
+        long score = ScoreForBluePrint(bp, 24);
+        totalScore += index * score;
+        index++;
+    }
+
+    Console.WriteLine(totalScore);
+}
+
+void Exercise2(string path)
+{
+    long totalScore = 1;
+    long index = 1;
+    foreach (var line in File.ReadLines(path).Take(3))
+    {
+        Console.WriteLine($"Calculating for {index}");
+
+        var bp = StringToBluePrint(line);
+        long score = ScoreForBluePrint(bp, 32);
+        totalScore *= score;
+        index++;
+    }
+
+    Console.WriteLine(totalScore);
+}
+
+string path = "../../../data2.txt";
+Exercise1(path);
+Exercise2(path);
