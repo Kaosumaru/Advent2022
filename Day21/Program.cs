3df1f5e8deb85c21768929df8953982a458168be
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-21 12:25:31 +0100

    FOrmating imporvements.

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,27 +1,24 @@
 ﻿using Day21;
 
-long Calculate(string path)
+Monkeys GetMonkeysFor(string path, bool equals)
 {
-    var monkeys = new Monkeys(false);
+    var monkeys = new Monkeys(equals);
     foreach (var line in File.ReadLines(path))
     {
         var s = line.Split(": ");
         monkeys.AddLine(s[0], s[1]);
     }
+    return monkeys;
+}
 
-    return monkeys.CalculateFor("root");
+long Calculate(string path)
+{
+    return GetMonkeysFor(path, false).CalculateFor("root");
 }
 
 long Solve(string path)
 {
-    var monkeys = new Monkeys(true);
-    foreach (var line in File.ReadLines(path))
-    {
-        var s = line.Split(": ");
-        monkeys.AddLine(s[0], s[1]);
-    }
-
-    return monkeys.SolveFor("root");
+    return GetMonkeysFor(path, true).SolveFor("root");
 }
 
 string path = "../../../data2.txt";
