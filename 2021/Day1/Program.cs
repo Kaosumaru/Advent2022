8f226aa748b569880cc510d13b71c73afe1782fc
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-29 21:13:37 +0100

    A

---
+++ 2021/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿string path = "../../../data2.txt";
+
+IEnumerable<int> SumOf(IEnumerable<int> l, int c)
+{
+    return Enumerable.Range(0, l.Count() - c + 1)
+        .Select(i => l.Skip(i).Take(c).Sum());
+}
+
+int CountIncreasing(IEnumerable<int> l)
+{
+    return l
+        .Zip(l.Skip(1))
+        .Where(t => t.First < t.Second)
+        .Count();
+}
+
+var lines = File.ReadLines(path).Select(l => int.Parse(l)).ToList();
+Console.WriteLine(CountIncreasing(lines));
+
+var normalizedLines = SumOf(lines, 3).ToList();
+Console.WriteLine(CountIncreasing(normalizedLines));
