d6d7d5d0a086724ef230a9313521fa638dce3df4
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-30 22:40:26 +0100

    Added day15 part1.

---
+++ 2021/Day15/Program.cs
@@ -0,0 +1,30 @@
+﻿using Utils;
+
+RiskMap CreateGridFromFile(string path)
+{
+    var lines = File.ReadAllLines(path);
+    var grid = new RiskMap(lines[0].Length, lines.Length);
+
+    foreach (var p in grid.AllPositions())
+    {
+        int value = lines[p.y][p.x] - '0';
+        grid.SetValue(p, value);
+    }
+
+    return grid;
+}
+
+void Exercise1(string path)
+{
+    var grid = CreateGridFromFile(path);
+
+    DjikstraPath djikstra = new();
+    var start = grid.GetNode(Vector2Int.Zero);
+    var end = grid.GetNode(new(grid.Width - 1, grid.Height - 1));
+    djikstra.FindPathTo(start);
+
+    Console.WriteLine(djikstra.GetDistance(end));
+}
+
+string path = "../../../data2.txt";
+Exercise1(path);
---
+++ 2021/Day15/RiskMap.cs
@@ -0,0 +1,22 @@
+﻿
+using Utils;
+
+class RiskMap : GenericIntGrid
+{
+    public RiskMap(int w, int h) : base(w, h)
+    {
+
+    }
+
+    public override IEnumerable<DjikstraNode.ConnectionInfo> GetConnectionsFor(Vector2Int p)
+    {
+        int v = GetValue(p);
+        return Neighbors4Of(p)
+            .Select(n =>
+            {
+                var node = GetNode(n);
+                return new DjikstraNode.ConnectionInfo { Distance = node.Value, Node = node };
+            });
+    }
+
+}
