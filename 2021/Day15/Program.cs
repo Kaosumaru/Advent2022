ee4133a81d9dc5eae02a385f8931ee73cf37a991
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-31 05:22:34 +0100

    Solved day15.

--- 2021/Day15/Program.cs
+++ 2021/Day15/Program.cs
@@ -1,4 +1,6 @@
-﻿RiskMap CreateGridFromFile(string path)
+﻿using Utils;
+
+RiskMap CreateGridFromFile(string path)
 {
     var lines = File.ReadAllLines(path);
     var grid = new RiskMap(lines[0].Length, lines.Length);
@@ -12,12 +14,38 @@
     return grid;
 }
 
+RiskMap DuplicateMap(RiskMap other, int size)
+{
+    var grid = new RiskMap(other.Width * size, other.Height * size);
+    for (int y = 0; y < size; y++)
+        for (int x = 0; x < size; x++)
+        {
+            var delta = new Vector2Int(x * other.Width, y * other.Height);
+            foreach (var pos in other.AllPositions())
+            {
+                int v = other.GetValue(pos) - 1;
+                v = MathUtils.WrapAround(v + x + y, 9) + 1;
+
+                grid.SetValue(delta + pos, v);
+            }
+        }
+    return grid;
+}
+
 void Exercise1(string path)
 {
     var grid = CreateGridFromFile(path);
-    var distance = grid.GetDistance(grid.TopLeft, grid.BottomRight);
+    var distance = grid.GetDistance(grid.BottomRight, grid.TopLeft);
+    Console.WriteLine(distance);
+}
+
+void Exercise2(string path)
+{
+    var grid = DuplicateMap(CreateGridFromFile(path), 5);
+    var distance = grid.GetDistance(grid.BottomRight, grid.TopLeft);
     Console.WriteLine(distance);
 }
 
 string path = "../../../data2.txt";
 Exercise1(path);
+Exercise2(path);
