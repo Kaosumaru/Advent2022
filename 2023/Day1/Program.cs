fa180151f43651591ff8721bd651f81f8e73cf26
Mateusz Borycki <mateuszborycki@gmail.com>
2023-12-09 22:21:20 +0100

    Adding initial versions of Day1 & 2

---
+++ 2023/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿int LineToNumber(string line)
+{
+    var numbers = line.Where(c => c >= '0' && c <= '9')
+        .Select(c => c - '0');
+    return numbers.First() * 10 + numbers.Last();
+}
+
+void Exercise1(string path)
+{
+    var sum = File.ReadLines(path)
+        .Select(LineToNumber)
+        .Sum();
+
+    Console.WriteLine(sum);
+}
+
+string path = "../../../data.txt";
+Exercise1(path);
