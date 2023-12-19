db8b324acedd7a131b726d64a7341e9f49eede2f
Mateusz Borycki <mateuszborycki@gmail.com>
2023-12-19 20:21:38 +0100

    Added day 1.5

---
+++ 2023/Day1/Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Numbers
+    {
+        static public Dictionary<string, int> Entries = new Dictionary<string, int>()
+        {
+            { "0", 0 },
+            { "zero", 0 },
+            { "1", 1 },
+            { "one", 1 },
+            { "2", 2 },
+            { "two", 2 },
+            { "3", 3 },
+            { "three", 3 },
+            { "4", 4 },
+            { "four", 4 },
+            { "5", 5 },
+            { "five", 5 },
+            { "6", 6 },
+            { "six", 6 },
+            { "7", 7 },
+            { "seven", 7 },
+            { "8", 8 },
+            { "eight", 8 },
+            { "9", 9 },
+            { "nine", 9 },
+        };
+    }
+}
--- 2023/Day1/Program.cs
+++ 2023/Day1/Program.cs
@@ -1,4 +1,6 @@
-﻿int LineToNumber(string line)
+﻿using Day1;
+
+int LineToNumber(string line)
 {
     var numbers = line.Where(c => c >= '0' && c <= '9')
         .Select(c => c - '0');
@@ -14,5 +16,40 @@ void Exercise1(string path)
     Console.WriteLine(sum);
 }
 
+string FirstFoundString(IEnumerable<string> searchFor, string text)
+{
+    return searchFor.Select(entry => (entry, text.IndexOf(entry)))
+        .Where(pair => pair.Item2 >= 0)
+        .MinBy(pair => pair.Item2).entry; ;
+}
+
+string LastFoundString(IEnumerable<string> searchFor, string text)
+{
+    return searchFor.Select(entry => (entry, text.LastIndexOf(entry)))
+        .Where(pair => pair.Item2 >= 0)
+        .MaxBy(pair => pair.Item2).entry;
+}
+
+int LineToNumber2(string line)
+{
+    var firstString = FirstFoundString(Numbers.Entries.Keys, line);
+    var lastString = LastFoundString(Numbers.Entries.Keys, line);
+
+    int firstNumber = Numbers.Entries[firstString];
+    int lastNumber = Numbers.Entries[lastString];
+
+    return firstNumber * 10 + lastNumber;
+}
+
+void Exercise2(string path)
+{
+    var sum = File.ReadLines(path)
+    .Select(LineToNumber2)
+    .Sum();
+
+    Console.WriteLine(sum);
+}
+
 string path = "../../../data.txt";
 Exercise1(path);
+Exercise2(path);
