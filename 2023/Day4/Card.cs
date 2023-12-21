e102a9aa47012cc431f6176be2f5bd7763027513
Mateusz Borycki <mateuszborycki@gmail.com>
2023-12-22 00:31:30 +0100

    Add solution for day 4.5

--- 2023/Day4/Card.cs
+++ 2023/Day4/Card.cs
@@ -1,40 +1,55 @@
 ﻿
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Reflection.Metadata.Ecma335;
+using Utils;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Collections;
 
 namespace Day3
 {
 
-    internal class Card
+    internal class CardSet
     {
-        public Card(List<int> winningNumbers, List<int> numbers)
-        {
-            this.winningNumbers = winningNumbers;
-            this.numbers = numbers;
-
-            winningNumbersSet = new HashSet<int>(winningNumbers);
+        public CardSet() 
+        { 
         }
 
-        public static Card FromString(string line)
+        public void Add(int id, Card card) 
+        {
+            cards[id] = card;
+        }
+
+        public Card Get(int id)
+        {
+            return cards.GetValueOrDefault(id);
+        }
+
+        public Dictionary<int, Card> cards = new();
+    }
+
+    internal class Card : GraphNode
+    {
+        public Card(CardSet set, int id, List<int> winningNumbers, List<int> numbers)
+        {
+            this.id = id;
+            set.Add(id, this);
+            cardSet = set;
+
+            HashSet<int> winningNumbersSet = new HashSet<int>(winningNumbers);
+            matches = numbers.Where(number => winningNumbersSet.Contains(number)).Count();
+        }
+
+        public static Card FromString(CardSet set, string line)
         {
             string pattern = @"Card\s+(\d*): (.*) \| (.*)";
             Regex rg = new(pattern);
             var mt = rg.Match(line);
 
-            Card card = new(TextToList(mt.Groups[2].Value), TextToList(mt.Groups[3].Value));
+            int id = int.Parse(mt.Groups[1].Value);
+
+            Card card = new(set, id, TextToList(mt.Groups[2].Value), TextToList(mt.Groups[3].Value));
             return card;
         }
 
-        public int Points()
-        {
-            int matches = numbers.Where(number => winningNumbersSet.Contains(number)).Count();
-            if (matches <= 1)
-                return matches;
-            return (int)Math.Pow(2, matches - 1);
-        }
 
         static List<int> TextToList(string text)
         {
@@ -44,9 +59,21 @@ namespace Day3
                 .ToList();
         }
 
-        List<int> winningNumbers;
-        List<int> numbers;
+        public IEnumerable<GraphNode.ConnectionInfo> GetConnections()
+        {
+            return Enumerable
+                .Range(id + 1, matches)
+                .Select(number => cardSet.Get(number))
+                .Where(card => card != null)
+                .Select(card => new GraphNode.ConnectionInfo()
+            {
+                Node = card,
+                Distance = 0,
+            });
+        }
 
-        HashSet<int> winningNumbersSet;
+        CardSet cardSet;
+        public int id;
+        public int matches;
     }
 }
--- 2023/Day4/Program.cs
+++ 2023/Day4/Program.cs
@@ -1,14 +1,41 @@
 ﻿using Day3;
+using Utils;
+
+int Points1(Card card)
+{
+    int matches = card.matches;
+    if (matches <= 1)
+        return matches;
+    return (int)Math.Pow(2, matches - 1);
+}
 
 void Exercise1(string path)
 {
+    CardSet set = new();
     var sum = File.ReadLines(path)
-        .Select(Card.FromString)
-        .Select(card => card.Points())
+        .Select(line => Card.FromString(set, line))
+        .Select(Points1)
         .Sum();
 
     Console.WriteLine(sum);
 }
 
-string path = "../../../data.txt";
+void Exercise2(string path)
+{
+    CardSet set = new();
+    var sumOfCards = File.ReadLines(path)
+        .Select(line => Card.FromString(set, line))
+        .Count();
+
+    foreach(var pair in set.cards)
+    {
+        sumOfCards += GraphUtils.IterateAllNodesRecursively(pair.Value).Count();
+    }
+
+    Console.WriteLine(sumOfCards);
+}
+
+
+string path = "../../../data2.txt";
 Exercise1(path);
+Exercise2(path);
