60ada846e54601782cecefadfa54a0843a3e1280
Mateusz Borycki <mateuszborycki@gmail.com>
2023-12-22 01:06:16 +0100

    Add day5

---
+++ 2023/Day5/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day5;
+using System.Numerics;
+
+Transformer CreateTransformer(string path, out List<long> input)
+{
+    using StreamReader file = new(path);
+    input = file.ReadLine()?.Split(" ").Skip(1).Select(text => long.Parse(text)).ToList();
+
+    Transformer transformer = new();
+    file.ReadLine();
+
+    while(!file.EndOfStream)
+    {
+        var name = file.ReadLine();
+        TransformerLayer layer = new(name);
+        transformer.AddLayer(layer);
+
+        while (true)
+        {
+            var line = file.ReadLine();
+            if (line == null || line == "") break;
+            var numbers = line.Split(" ").Select(text => long.Parse(text)).ToList();
+            layer.AddRange(numbers[0], numbers[1], numbers[2]);
+        }
+
+
+    }
+
+    return transformer;
+}
+
+
+void Exercise1(Transformer transformer, List<long> input)
+{
+    var lowest = input.Select(number => transformer.Transform(number)).Min();
+    Console.WriteLine(lowest);
+}
+
+string path = "../../../data2.txt";
+var transformer = CreateTransformer(path, out var input);
+
+Exercise1(transformer, input);
---
+++ 2023/Day5/Transformer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Transformer
+    {
+        public void AddLayer(TransformerLayer layer)
+        {
+            layers.Add(layer);
+        }
+
+        public long Transform(long input)
+        {
+            foreach(var layer in layers)
+            {
+                input = layer.Transform(input);
+            }
+            return input;
+        }
+
+
+        List<TransformerLayer> layers = new();
+    }
+
+    internal class TransformerLayer
+    {
+        public TransformerLayer(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddRange(long destination, long source, long length)
+        {
+            ranges.Add(new Range
+            {
+                Source = source,
+                Destination = destination,
+                Length = length
+            });
+        }
+
+        public long Transform(long input)
+        {
+            foreach (Range range in ranges)
+            {
+                var output = range.TryToMatch(input);
+                if (output.HasValue)
+                    return output.Value;
+            }
+            return input;
+        }
+
+        struct Range
+        {
+            public long Source;
+            public long Destination;
+            public long Length;
+
+            public long? TryToMatch(long input)
+            {
+                long delta = input - Source;
+                if (delta < 0)
+                    return null;
+                if (delta > Length - 1)
+                    return null;
+                return Destination + delta;
+            }
+        }
+
+        List<Range> ranges = new();
+        string name;
+    }
+}
